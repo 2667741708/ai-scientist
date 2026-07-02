38d52885ecbff36d75680a256bc59198af5e5abb
whm <2667741708@qq.com>
2026-07-02 12:35:37 +0800

    core: expose agent registry package

---
+++ src/open_coscientist/agents/__init__.py
@@ -0,0 +1,13 @@
+from open_coscientist.agents.registry import (
+    AgentSpec,
+    get_agent_registry_payload,
+    get_agent_spec,
+    list_agent_specs,
+)
+
+__all__ = [
+    "AgentSpec",
+    "get_agent_registry_payload",
+    "get_agent_spec",
+    "list_agent_specs",
+]
